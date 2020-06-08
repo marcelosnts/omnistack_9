2cde0ed4fea5604e9ded455c333fd6d53c274c92
Marcelo <santosmarcelo1995@live.com>
2020-06-08 10:03:28 -0300

    Upload completo - Semana Omnistack 9

---
+++ backend/src/routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
+//req = requisição | res = reposta
+//req.query = Acessar Query Params (Parametros da requisição [Filter])
+//req.params = Acessar Route Params (Parametros da URL [Edit, Delete])
+//req.body = Acessar corpo da requisição (CRUD)
+
+routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections', RejectionController.store);
+
+module.exports = routes;
